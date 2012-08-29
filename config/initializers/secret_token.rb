18a5065234665dee58cfbc0beb81e6f6d05e97d7
Chris Kühl <blixtra@gmail.com>
2012-08-29 13:38:34 +0200

    Initial commit after 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseOnAjax::Application.config.secret_token = '4f46b5701545ce966bf62581fad11f16b7a368916b2ef9a6e420fa91bbf8310aff09580f3cef4fa94082ec2dcc9c1f06e6f1007fe6bf61cc3a8344380ff6e86d'
