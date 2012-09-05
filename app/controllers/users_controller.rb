f0c6fe8c250c29ae091f55b7199000f824121f7f
Chris Kühl <blixtra@gmail.com>
2012-09-05 02:49:15 +0200

    Ajaxify two-phase registration process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
-  respond_to :html
+  respond_to :js
 
   def favorite_word
-    user = User.find params[:user_id]
-    if user.update_attributes(params[:user])
-      redirect_to :back
+    @user = User.find params[:user_id]
+    if @user.update_attributes(params[:user])
+      respond_with @user
     else
-      render :text => 'Oops'
+      render :text => 'Oops!'
     end
   end
 end
